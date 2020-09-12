72d8fdeccb30d4a93656af91c4d90ee1abec3104
Juan Igor <juan.igor@gdeste.ifce.edu.br>
2020-09-12 14:42:00 -0300

    Updated index routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import PageNotFound from './pages/PageNotFound';
+
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AnxietyExercises from './pages/AnxietyExercises';
+import SupportACause from './pages/SupportACause';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route exact path="/" render={(props) => (<Home {...props} />)} />
+      <Route exact path="/exercicios" render={(props) => (<AnxietyExercises {...props} />)} />
+      <Route exact path="/apoie-uma-causa" render={(props) => (<SupportACause {...props} />)} />
+      <Route path="*" render={(props) => (<PageNotFound {...props} />)} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
