8387f668e5c77b0ff1a31e8bc5d8addfa6fd0cd6
Juan Igor <juan.igor@gdeste.ifce.edu.br>
2020-09-12 13:57:23 -0300

    Added routes file

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { HomeRounded } from '@material-ui/icons';
+
+const routes = [
+    {
+        'header': 'Navegação',
+        'content': [
+            {
+                path: "/",
+                name: "Início",
+                icon: <HomeRounded />,
+            },
+            {
+                path: "/exercicios",
+                name: "Exercícios de Ajuda",
+                icon: <i class="fas fa-hands-helping"></i>
+            },
+            {
+                path: "/apoie-uma-causa",
+                name: "Apoie uma Causa",
+                icon: <i class="fas fa-hand-holding-heart"></i>,
+            }
+        ]
+    },
+  ];
+  
+  export default routes;
