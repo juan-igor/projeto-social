9a261b9026b357aad30321d628951801772c45e6
Juan Igor <juan.igor@gdeste.ifce.edu.br>
2020-09-13 17:42:04 -0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from './pages/Home';
-import PageNotFound from './pages/PageNotFound';
-
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+import Home from './pages/Home';
+import PageNotFound from './pages/PageNotFound';
 import AnxietyExercises from './pages/AnxietyExercises';
 import SupportACause from './pages/SupportACause';
+import AboutAnxiety from './pages/AboutAnxiety';
 
 ReactDOM.render(
   <Router>
@@ -16,6 +17,7 @@ ReactDOM.render(
       <Route exact path="/" render={(props) => (<Home {...props} />)} />
       <Route exact path="/exercicios" render={(props) => (<AnxietyExercises {...props} />)} />
       <Route exact path="/apoie-uma-causa" render={(props) => (<SupportACause {...props} />)} />
+      <Route exact path="/sobre-ansiedade" render={(props) => (<AboutAnxiety {...props} />)} />
       <Route path="*" render={(props) => (<PageNotFound {...props} />)} />
     </Switch>
   </Router>,
